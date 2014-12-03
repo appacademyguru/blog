1db6ff4362f07f7c139acad3a78c712d8db45fe6
Megan McCreight <appacademy.megan@gmail.com>
2014-12-03 15:05:07 -0800

    12/3/14 - videos 14-15

--- controller/create-db.php
+++ controller/create-db.php
@@ -4,7 +4,7 @@ require_once(__DIR__ . "/../model/database.php");
 $connection = new mysqli($host, $username, $password);
 //checks connection and puts error/success connection
 if($connection->connect_error){
-  die("Error: ".$connection->connect_error);
+  die("<p>Error: ".$connection->connect_error . "</p>");
 }
 $exists = $connection->select_db($database);
 //checks if the database exists
@@ -13,12 +13,25 @@ if(!$exists){
   $query = $connection->query("CREATE DATABASE $database");
   
   if($query){
-    "Successfully created database: ". database;
+    echo "<p>Successfully created database: ". database . "</p>";
   }
 }
 //if it does, echoes message
 else{
-  echo "Database already exists";
+  echo "<p>Database already exists.</p>";
+}
+//create table to store posts and ids
+$query = $connection->query("CREATE TABLE posts ("
+        . "id int(11) NOT NULL AUTO_INCREMENT,"
+        . "title varchar(255) NOT NULL,"
+        . "post text NOT NULL,"
+        . "PRIMARY KEY (id))");
+
+if($query){
+  echo "<p>Successfully created table: posts</p>";
+}
+else{
+  echo "<p>$connection->error</p>";
 }
 
 $connection->close();
